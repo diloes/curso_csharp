0a0da2656a81bf5d40ef1bb9b73b86617678776e
diloes <diloes@hotmail.com>
2022-07-10 22:27:21 +0200

    Sesion 1 Ejercicios resueltos

---
+++ sesion_1-ejercicios/Program.cs
@@ -0,0 +1,8 @@
+﻿// Ejercicio 1 - Escribe un programa que reciba tu nombre y lo escriba por consola.
+Console.WriteLine("Introduce tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola " + nombre);
+
+// Ejercicio 2 - Escribe un programa que tome la hora y la escriba por consola.
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine(datetime);
