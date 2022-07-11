ac54e2ec96bba6664ba64e364bf422fe4915ca21
diloes <diloes@hotmail.com>
2022-07-11 21:31:34 +0200

    Sesion 2 finalizada

---
+++ sesion_2_clase_2parte/Program.cs
@@ -0,0 +1,38 @@
+﻿/* SESION 2 - OPERADORES */
+
+// Operadores aritméticos + - * / ++ -- %
+int i = 0;
+Console.WriteLine("Suma de " + i + " + 1 es igual a " + (i + 1)); 
+Console.WriteLine("Autoincremento " + ++i);
+Console.WriteLine("División " + i / 4); // el resultado será entero
+
+// Operadores de asignación
+/* Operator  Example    Same as
+*    =	      x = 5	     x = 5	
+*   -=	      x -= 3	   x = x - 3	
+*   +=	      x += 3	   x = x + 3	
+*   *=	      x *= 3	   x = x * 3	
+*   /=	      x /= 3	   x = x / 3	
+*   %=	      x %= 3	   x = x % 3	
+*   &=	      x &= 3	   x = x & 3	
+*   |=	      x |= 3	   x = x | 3	
+*   ^=	      x ^= 3	   x = x ^ 3	
+*   >>=	      x >>= 3	   x = x >> 3	
+*   <<=	      x <<= 3	   x = x << 3
+*/
+
+// Operadores de comparación
+/*
+*  menor que -> <
+*  mayor que -> >
+*  igual que -> ==
+*  no iguale que -> !=
+*/
+
+// Operadores lógicos
+/*
+* Y -> && 
+* OR -> || 
+* NOT -> !
+*/
+// Esto es igual que en Javascript. Por eso no hago ejemplos con esto.
