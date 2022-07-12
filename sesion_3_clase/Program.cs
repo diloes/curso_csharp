36b9582dba39f12d0a97a19c8fb15ad78a318783
diloes <diloes@hotmail.com>
2022-07-12 17:08:38 +0200

    Sesion 3 clase terminada

---
+++ sesion_3_clase/Program.cs
@@ -0,0 +1,22 @@
+﻿/* SESION 3 - ESTRUCTURAS */
+// Creamos un objeto de la struc Coords
+Coords misCoordenadas = new Coords(23.93382, -12.12345);
+Console.WriteLine(misCoordenadas.X);
+Console.WriteLine(misCoordenadas.Y);
+
+// Creamos un objeto similar al anterior pero cambiando un valor
+Coords misCoodernadas2 = misCoordenadas with { X = 06.34562 };
+
+public struct Coords
+{
+    public Coords(double x, double y)
+    {
+      X = x;
+      Y = y;
+    }
+    public double X { get; set; }
+    public double Y { get; }
+
+    public override string ToString() => $"{X} {Y}";
+
+}
