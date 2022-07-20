413328bd66daf0b12f8ad96d19160476d022678b
diloes <diloes@hotmail.com>
2022-07-20 19:53:53 +0200

    Sesion 11 - Generics

---
+++ sesion_11_generics/Program.cs
@@ -0,0 +1,15 @@
+﻿/* SESION 11 - Generics */
+
+/* GENERIC */
+Generic<string> str = new Generic<string>();
+str.Dato = "dato";
+Console.WriteLine($"Generic dato: {str.Dato}");
+Console.WriteLine($"Tipo de dato: {str.Dato.GetType().FullName}");
+Console.WriteLine($"Tipo de clase: {str.GetType()}");
+
+// Esto es una clase generica donde en T le indicaremos el tipo de dato con el que trabajaremos cuando la usemos
+public class Generic<T>
+{
+  public T Dato { get; set; }
+}
+
