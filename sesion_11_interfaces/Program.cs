aafa00bad05f1cae0e0cee0a5211e3a535cf6fb7
diloes <diloes@hotmail.com>
2022-07-22 10:11:55 +0200

    Sesion 11 - Interfaces

---
+++ sesion_11_interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿/* SESION 11 - INTERFACES */
+// Una interfaz es una clase que define atributos, métodos, etc. sin implementarlos.
+
+Vehiculo vehiculo = new Vehiculo();
+vehiculo.Marca = "Ford";
+
+interface IVehiculo
+{
+  string Marca { get; set; }
+  void Arrancar();
+}
+
+class Vehiculo : IVehiculo
+{
+  public string Marca { get; set; }
+  void IVehiculo.Arrancar()
+  {
+    Console.WriteLine("Arrancar");
+  }
+}
