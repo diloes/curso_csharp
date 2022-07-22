18ebc9310adb8d9288a4182b2b1359af42a2c45d
diloes <diloes@hotmail.com>
2022-07-22 09:45:01 +0200

    Sesion 11 - Herencia

---
+++ sesion_11_herencia/Program.cs
@@ -0,0 +1,35 @@
+﻿/* SESION 11 - Herencia */
+
+Vehiculo vehiculo = new Vehiculo();
+vehiculo.Arrancar();
+
+Coche coche = new Coche();
+coche.Arrancar(); // Podemos acceder a este metodo porque hemos heredado de la clase Vehiculo
+
+// Clase padre
+class Vehiculo
+{
+  /* Ya no es necesario crear un atributo y después el metodo para modificarlo o obtnerlo.
+  Ahora declaramos directamente como propiedad con get y set. */
+  public float Combustible { get; set; }
+  public int Aforo { get; set; }
+  public void Arrancar()
+  {
+    Console.WriteLine("Arrancaaa");
+  }
+}
+
+// Clase hija que hereda de Vehiculo
+class Coche : Vehiculo
+{
+  public int Ruedas { get; set; }
+  public void DetenerCoche()
+  {
+    Console.WriteLine("Para");
+  }
+  // Se pueden cambiar los metodos que heredamos de la clase padre.
+  public void Arrancar()
+  {
+    Console.WriteLine("Arranca Coche");
+  }
+}
