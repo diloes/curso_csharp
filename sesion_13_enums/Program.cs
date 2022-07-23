4ec13be8f21ce88c044308a274ca5fdf014894a0
diloes <diloes@hotmail.com>
2022-07-23 11:04:49 +0200

    Sesion 13 - enums

---
+++ sesion_13_enums/Program.cs
@@ -0,0 +1,21 @@
+﻿/* SESION 13 - ENUMS */
+// Se usan cuando son constantes. Ej: Estaciones, días de semana, meses, etc.
+
+Console.WriteLine(Estaciones.Otoño);
+Console.WriteLine((int)CodigosError.Conexion);
+
+enum Estaciones
+{
+  Verano,
+  Primavera,
+  Otoño,
+  Invierno
+}
+
+enum CodigosError : ushort
+{
+  Ninguno = 0,
+  Descononido = 1,
+  SinConexion = 100,
+  Conexion = 200
+}
